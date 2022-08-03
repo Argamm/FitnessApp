72a3cf5a315b5d9aabada1b6ae74f640574c07db
Argamm <mehrabyan.argam@mail.ru>
2022-08-04 00:54:01 +0400

    FeedbackFragment.kta and RateUsFragment.kt are ready to using

--- app/src/main/java/com/example/fitnessapp/fragment/navigation/myProfile/FeedbackFragment.kt
+++ app/src/main/java/com/example/fitnessapp/fragment/navigation/myProfile/FeedbackFragment.kt
@@ -1,11 +1,14 @@
 package com.example.myfitnessapp.fragment.navigation.myProfile
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.fitnessapp.R
+import kotlinx.android.synthetic.main.fragment_feedback.*
 
 class FeedbackFragment:Fragment() {
     override fun onCreateView(
@@ -14,5 +17,25 @@ class FeedbackFragment:Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_feedback, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        btn_send_message.setOnClickListener {
+            val text = edt_feedback.text.toString()
+            val intent = Intent()
+            val gmail = "mehrabyanargam111@gmail.com"
+
+
+            if (text.isNotEmpty()) {
+                intent.action = Intent.ACTION_SENDTO
+                intent.putExtra(Intent.EXTRA_EMAIL, gmail)
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback")
+                intent.putExtra(Intent.EXTRA_TEXT, text)
+//            intent.type = "message/rfc822"
+                intent.data = Uri.parse("mailto:${gmail}")
+                startActivity(intent)
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/fitnessapp/fragment/navigation/myProfile/RateUsFragment.kt
+++ app/src/main/java/com/example/fitnessapp/fragment/navigation/myProfile/RateUsFragment.kt
@@ -1,18 +1,38 @@
 package com.example.myfitnessapp.fragment.navigation.myProfile
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.fitnessapp.R
+import kotlinx.android.synthetic.main.fragment_rate_us.*
 
-class RateUsFragment:Fragment() {
+
+class RateUsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_rate_us, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val totalStars = "Total Stars:: " + rating.numStars
+        val ratingA = "Rating :: " + rating.rating
+
+
+        btn_rate.setOnClickListener {
+            Toast.makeText(activity,"""
+                        $totalStars
+                        $ratingA
+                        """.trimIndent(), Toast.LENGTH_LONG
+            ).show()
+            btn_rate.isEnabled = false
+            rating.isEnabled = false
+        }
     }
 }
